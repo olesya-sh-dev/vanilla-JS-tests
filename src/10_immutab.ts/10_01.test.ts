3c537d66ba2e255664777ca498a83d2fbe9d4b53
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-04-03 10:36:53 +0300

    глубокое копирование массивов примитивов с помощью map

--- src/10_immutab.ts/10_01.test.ts
+++ src/10_immutab.ts/10_01.test.ts
@@ -1,8 +1,12 @@
 import {
   UserType,
+  UserWithBooksType,
   UserWithLapTopType,
+  addNewBooksToUser,
   makeHairStyle,
   moveUser,
+  moveUserToOtherHouse,
+  updateBook,
   upgradeUserLaptop,
 } from "./10_01";
 
@@ -66,4 +70,82 @@ test("upgrade laptop to macbook", () => {
   expect(user.laptop).not.toBe(upgradedUser.laptop);
   expect(user.laptop.title).toBe("ZenBook");
   expect(user.address).toBe(upgradedUser.address);
+});
+
+test("upgrade house number", () => {
+  let user: UserWithLapTopType & UserWithBooksType = {
+    name: "Dimych",
+    hair: 32,
+    address: {
+      city: "Minsk",
+      house: 12,
+    },
+    laptop: {
+      title: "ZenBook",
+    },
+    books: ["css", "html", "js", "react"],
+  };
+
+  const userCopy = moveUserToOtherHouse(user, 8);
+
+  expect(user).not.toBe(userCopy);
+  expect(user.laptop.title).toBe(userCopy.laptop.title);
+  expect(user.address).not.toBe(userCopy.address);
+  expect(user.laptop.title).toBe("ZenBook");
+  expect(user.address.city).toBe(userCopy.address.city);
+  expect(userCopy.address.house).toBe(8);
+  expect(user.address.house).not.toBe(userCopy.address.house);
+});
+
+test("add new books to user", () => {
+  let user: UserWithLapTopType & UserWithBooksType = {
+    name: "Dimych",
+    hair: 32,
+    address: {
+      city: "Minsk",
+      house: 12,
+    },
+    laptop: {
+      title: "ZenBook",
+    },
+    books: ["css", "html", "js", "react"],
+  };
+
+  const userCopy = addNewBooksToUser(user, ["ts", "angular"]);
+
+  expect(user).not.toBe(userCopy);
+  expect(user.laptop.title).toBe(userCopy.laptop.title);
+  expect(user.address).toBe(userCopy.address);
+  expect(userCopy.address.house).toBe(12);
+  expect(user.books).not.toBe(userCopy.books);
+  expect(user.books.length).toBe(4);
+  expect(userCopy.books.length).toBe(6);
+  expect(userCopy.books[4]).toBe("ts");
+  expect(userCopy.books[5]).toBe("angular");
+});
+
+test("update book", () => {
+  let user: UserWithLapTopType & UserWithBooksType = {
+    name: "Dimych",
+    hair: 32,
+    address: {
+      city: "Minsk",
+      house: 12,
+    },
+    laptop: {
+      title: "ZenBook",
+    },
+    books: ["css", "html", "js", "react"],
+  };
+
+  const userCopy = updateBook(user, "js", "ts");
+
+  expect(user).not.toBe(userCopy);
+  expect(user.laptop.title).toBe(userCopy.laptop.title);
+  expect(user.address).toBe(userCopy.address);
+  expect(userCopy.address.house).toBe(12);
+  expect(user.books).not.toBe(userCopy.books);
+  expect(user.books.length).toBe(4);
+  expect(userCopy.books.length).toBe(4);
+  expect(userCopy.books[2]).toBe("ts");
 });
--- src/10_immutab.ts/10_01.tsx
+++ src/10_immutab.ts/10_01.tsx
@@ -11,6 +11,10 @@ export type LapTopType = {
 export type UserWithLapTopType = UserType & {
   laptop: LapTopType;
 };
+export type UserWithBooksType = UserType & {
+  books: string[];
+};
+
 export function makeHairStyle(u: UserType, power: number) {
   const copy = { ...u, hair: u.hair / power };
   //copy.hair = u.hair / power;
@@ -25,7 +29,29 @@ export function moveUser(u: UserWithLapTopType, city: string) {
 
   // return copy;
 }
+export function moveUserToOtherHouse(
+  u: UserWithLapTopType & UserWithBooksType,
+  house: number
+) {
+  return { ...u, address: { ...u.address, house: house } };
+}
 
 export function upgradeUserLaptop(u: UserWithLapTopType, newTitle: string) {
   return { ...u, laptop: { ...u.laptop, title: newTitle } };
+}
+
+export function addNewBooksToUser(
+  u: UserWithLapTopType & UserWithBooksType,
+  newBooks: string[]
+) {
+  // return { ...u, books: [...u.books, ...newBooks] };
+  return { ...u, books: u.books.concat(newBooks) };
+}
+export function updateBook(
+  u: UserWithLapTopType & UserWithBooksType,
+  book1: string,
+  book2: string
+) {
+  // return { ...u, books: [...u.books, ...newBooks] };
+  return { ...u, books: u.books.map((b) => (b === book1 ? book2 : b)) };
 }
